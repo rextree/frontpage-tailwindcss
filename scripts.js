fc494c668e32821ec9683999e9d70121775c346d
Rex <rexyu24@gmail.com>
2021-07-12 03:30:23 +1200

    initialise git

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+    document.querySelector('.menu').classList.toggle('menu-btn-toggle');
+})
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    '*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        'body': 'Barlow, Helvetica, Ariel'
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
